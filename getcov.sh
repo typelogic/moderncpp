50ffc6e89acdac9dac6bc4bff1c8d362a752e019
truth <truthadjustr@gmail.com>
2019-08-01 22:18:05 +0800

    automated coverage display

---
+++ getcov.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+getcov () { 
+    local tmpdir=$(mktemp -d)
+    for f in *.gcno
+    do
+        gcov $f
+    done
+    lcov --capture --directory . --output-file result.info
+    mkdir -p $tmpdir
+    genhtml result.info --output-directory $tmpdir
+    pushd $tmpdir/
+    echo "[$tmpdir]"
+    python2.7 -m SimpleHTTPServer
+    popd
+}
+
+getcov
